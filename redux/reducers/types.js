3a5b398e63c368758d215f1ccd807984c2daffd6
Fatih Muhamad Ridho <fatihmuhamadridho@192.168.1.3>
2021-10-14 08:36:59 +0700

    test

--- redux/reducers/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_PRODUCTS = "GET_PRODUCTS";
-export const PRODUCTS_ERROR = "PRODUCTS_ERROR";
-export const ADD_PRODUCTS = "ADD_PRODUCTS";
-export const EDIT_PRODUCTS = "EDIT_PRODUCTS";
-export const DELETE_PRODUCTS = "DELETE_PRODUCTS";
