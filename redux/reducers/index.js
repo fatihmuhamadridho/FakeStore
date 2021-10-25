7bede0302aa0e2c84eeeaeb4d39e5b9b83fffbb1
Fatih Muhamad Ridho <fatihmuhamadridho@192.168.1.6>
2021-10-25 11:20:09 +0700

    delete

--- redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import productReducer from "./productReducers";
-import cartReducer from "./cartReducers";
-
-export default combineReducers({
-  Products: productReducer,
-  Carts: cartReducer,
-});
